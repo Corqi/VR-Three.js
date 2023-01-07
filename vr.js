aa895d806b130bc423b8052383cb89c5135ed722
Corqi <kowalikm@student.agh.edu.pl>
2023-01-07 01:44:05 +0100

    bug fix

--- vr.js
+++ vr.js
@@ -8,7 +8,7 @@ function main() {
   const canvas = document.querySelector('#c');
   const renderer = new THREE.WebGLRenderer({canvas});
   renderer.xr.enabled = true;
-  renderer.xr.setReferenceSpaceType( 'local' );
+  renderer.xr.setReferenceSpaceType( 'bounded-floor' );
   document.body.appendChild(VRButton.createButton(renderer));
 
   const fov = 75;
